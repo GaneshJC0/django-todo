ec2fe2dad82f73545a3f50b99185ed5b1c60cbc8
GaneshJC0 <ganeshjchoudhari@gmail.com>
2024-09-18 17:30:04 +0530

    scripts change

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/django-todo/todoApp/settings.py
+sed -i 's/\[]/\["15.206.174.186"]/' /home/ubuntu/django-todo/todoApp/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
